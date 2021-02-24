7f95c472cabca5fc54f971b21e8cd228927c9e58
Stuart Cray <>
2021-02-24 14:33:23 +0000

    Remove book-common dependency.

---
+++ src/main/java/com/stucray/bookstore/common/model/Book.java
@@ -0,0 +1,22 @@
+package com.stucray.bookstore.common.model;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Book {
+
+    private String id;
+    private String googleId;
+    private String title;
+    private String subtitle;
+    private String description;
+    private String publisher;
+    private String publishedDate;
+    private List<String> authors = new ArrayList<>();
+    private List<String> categories;
+    private String thumbnail;
+    private String smallThumbnail;
+}
